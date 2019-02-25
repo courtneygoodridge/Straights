8d99522aef6d0f8543232a446cc5b89dc2eb7348
courtneygoodridge <pscmgo@leeds.ac.uk>
2019-02-25 11:17:17 +0000

    back to 10 trials for pilot - 15 trials threw an error in the code

--- Ben-Lui_beta_main.py
+++ Ben-Lui_beta_main.py
@@ -1,4 +1,4 @@
- 
+1 
 """
 Script to run threshold vs accumulator experiment. The participant experiences vection across a textured ground-plane. 
 After a few seconds a straight road appears with a experimentally controlled deflection angle. The participants task is to steer so as to try and stay on the straight road.
@@ -334,7 +334,7 @@ class myExperiment(viz.EventClass):
 		#self.FACTOR_headingpool = np.linspace(-30, 30, 5) #array from -45 to 45. 
 		#self.FACTOR_occlPool = [0, .5, 1] #3 occlusion delay time conditions
 		self.FACTOR_occlPool = [0] #3 occlusion delay time conditions
-		self.TrialsPerCondition = 15 # was oriringally 10	
+		self.TrialsPerCondition = 10 # was oriringally 10	
 		[trialsequence_signed, cl_heading, cl_occl]  = GenerateConditionLists(self.FACTOR_headingpool, self.FACTOR_occlPool, self.TrialsPerCondition)
 
 		self.TRIALSEQ_signed = trialsequence_signed #list of trialtypes in a randomised order. -ve = leftwards, +ve = rightwards.

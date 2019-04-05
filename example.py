bc8276e153c54b07037efb8fcdf6e62a8e0385e5
courtneygoodridge <courtneygoodridge96@gmail.com>
2019-04-05 20:01:14 +0100

    playing around with jupyter notebook

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv ('M:\PhD\Project\Experiment_Code\Straights\BenLui17_1.csv')
+df = pd.read_csv ("C:/Users/Courtney/Documents/PhD/Project/Experiment_code/Straights/BenLui17_1.csv")
 print (df)
 
 timestamp = df[['timestamp']]

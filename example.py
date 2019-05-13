ee2a3cb01686da33b23586ce76e68135b98e424c
courtneygoodridge <courtneygoodridge96@gmail.com>
2019-05-13 22:43:32 +0100

    uniting ppid and trialn - R -> Py

--- example.py
+++ example.py
@@ -1,12 +1,17 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv ("M:\PhD\Project\Experiment_Code\Straights\BenLui17_1_full.csv")
-# df = pd.read_csv ("C:/Users/Courtney/Documents/PhD/Project/Experiment_code/Straights/BenLui17_1_full.csv")
-print (df)
-timestamp = df[['timestamp']]
+import pandas as pd # imports dataframes
+import matplotlib.pyplot as plt #imports plotting capabilities
+# workingdata = pd.read_csv ("M:/PhD/Project/Experiment_Code/Straights/BenLui17_1_full.csv")
+workingdata = pd.read_csv("C:/Users/Courtney/Documents/PhD\Project/Experiment_code/Straights/BenLui17_1_full.csv") # home laptop data reading
+print (workingdata)
+
+workingdata['ppid_trialn'] = workingdata.ppid.astype(str).str.cat(workingdata.trialn.astype(str), sep ='_')
+
+ # practice - selects timestamp column
+timestamp = workingdata[['timestamp']]
 print(timestamp)
 
-df.plot(kind='scatter',x='timestamp',y='SWA',color='red')
+# practicie - red scatter plot of timestamp versus steering wheel angle 
+workingdata.plot(kind='scatter',x='timestamp',y='SWA',color='red')
 plt.show()
 
 

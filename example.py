4a8b6c0d0d70be9d2e937b4ab9389189d6b28bc2
courtneygoodridge <pscmgo@leeds.ac.uk>
2019-05-14 17:04:42 +0100

    phd laptop set up for python analysis

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import pandas as pd # imports dataframes
 import matplotlib.pyplot as plt #imports plotting capabilities
-# workingdata = pd.read_csv ("M:/PhD/Project/Experiment_Code/Straights/BenLui17_1_full.csv")
-workingdata = pd.read_csv("C:/Users/Courtney/Documents/PhD\Project/Experiment_code/Straights/BenLui17_1_full.csv") # home laptop data reading
+workingdata = pd.read_csv ("M:/PhD/Project/Experiment_Code/Straights/BenLui17_1_full.csv")
+# workingdata = pd.read_csv("C:/Users/Courtney/Documents/PhD\Project/Experiment_code/Straights/BenLui17_1_full.csv") # home laptop data reading
 print (workingdata)
 
 workingdata['ppid_trialn'] = workingdata.ppid.astype(str).str.cat(workingdata.trialn.astype(str), sep ='_')

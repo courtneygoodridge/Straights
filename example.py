d47aafe716f49f726ccb8e4b0e6c421e15b56366
courtneygoodridge <pscmgo@leeds.ac.uk>
2019-05-16 18:04:07 +0100

    loading multiple csvs in  python

--- example.py
+++ example.py
@@ -1,17 +1,39 @@
 import pandas as pd # imports dataframes
 import matplotlib.pyplot as plt #imports plotting capabilities
-workingdata = pd.read_csv ("M:/PhD/Project/Experiment_Code/Straights/BenLui17_1_full.csv")
-# workingdata = pd.read_csv("C:/Users/Courtney/Documents/PhD\Project/Experiment_code/Straights/BenLui17_1_full.csv") # home laptop data reading
-print (workingdata)
+import glob
+
+path = r"M:/PhD/Project/Experiment_Code/Straights/Full_Data" # use your path
+all_files = glob.glob(path + "/*.csv") # select path and file identifier 
+
+li = [] # empty for csv files to be put
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col = None, header = 0) # read in the csv files
+    li.append(df) #append the files 
+
+workingdata = pd.concat(li, axis = 0, ignore_index = True) # concatenate all of the data 
 
 workingdata['ppid_trialn'] = workingdata.ppid.astype(str).str.cat(workingdata.trialn.astype(str), sep ='_')
+# unite ppid and trialn column
 
- # practice - selects timestamp column
-timestamp = workingdata[['timestamp']]
-print(timestamp)
+del workingdata['X.1'] # delete unnecessary columns
 
-# practicie - red scatter plot of timestamp versus steering wheel angle 
-workingdata.plot(kind='scatter',x='timestamp',y='SWA',color='red')
-plt.show()
+
+
+
+
+
+
+
+
+
+
+### practice - selects timestamp column
+# timestamp = workingdata[['timestamp']]
+# print(timestamp)
+
+### practicie - red scatter plot of timestamp versus steering wheel angle 
+# workingdata.plot(kind='scatter',x='timestamp',y='SWA',color='red')
+# plt.show()
 
 
